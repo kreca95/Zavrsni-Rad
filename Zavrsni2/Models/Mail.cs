3e72ba304c22c16356ce65d3e3ff1a4452f27a72
kreca95 <43502736+kreca95@users.noreply.github.com>
2019-02-26 22:49:15 +0100

    Update Mail.cs

--- Zavrsni2/Models/Mail.cs
+++ Zavrsni2/Models/Mail.cs
@@ -11,8 +11,8 @@ namespace Zavrsni2.Models
         public static int SmtpPort { get; } = 587;
         public static bool SmtpUseDefaultCredentials { get; } = true;
         public static bool EnableSsl { get;} = true;
-        public static string MojMail { get;} = "mailzaskidanjenaposlu@gmail.com";
-        public static string Sifra { get; } = "88360210q";
+        public static string MojMail { get;} = "";
+        public static string Sifra { get; } = "";
         public static string PrimalacMaila { get; }
         public static string Subject { get; } = "Pred racun";
 
